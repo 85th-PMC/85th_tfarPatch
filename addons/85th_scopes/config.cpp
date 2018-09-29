1f9e042c128eff4dbe945e4d40ad367251cf0dea
Snak3Doc <benjaminbadham@gmail.com>
2018-09-29 20:33:02 +1000

    Adds test config
    Not sure if this is the correct format for doing this (needs revision) but should provide a basis for future additions.

--- addons/85th_scopes/config.cpp
+++ addons/85th_scopes/config.cpp
@@ -1,1 +1,26 @@
-a
+class CfgPatches
+{
+	class 85th_scopes
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Characters_F"
+		};
+        author="85th PMC"
+        authors[]=
+		{
+			"SnakeDoc"
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class B_Kitbag_Base;
+	{
+		scope=1;
+	};
+};
